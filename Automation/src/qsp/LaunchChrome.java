e5d8b20398f481f5bd353b19fe7db3fc496253e5
akanksha-2020 <90815574+akanksha-2020@users.noreply.github.com>
2021-09-17 09:31:23 +0530

    Update LaunchChrome.java

--- Automation/src/qsp/LaunchChrome.java
+++ Automation/src/qsp/LaunchChrome.java
@@ -7,6 +7,7 @@ public class LaunchChrome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		System.out.println("Hello");
  new ChromeDriver();
 
 	}
